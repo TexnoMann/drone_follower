a4b13158f6db4f74d059b66fb9f53799d6332f31
TexnoMann <electrodans@yandex.ru>
2019-01-26 22:52:05 +0300

    fix depend

--- ardrone_system/src/test_cv.cpp
+++ ardrone_system/src/test_cv.cpp
@@ -11,6 +11,7 @@ cv::Scalar circleColor(255, 255, 255), contourColor(0, 255, 0);
 int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "test_drone_cv");
+    drone ar;
     ros::NodeHandle nh;
     ros::AsyncSpinner spinner(2);
     spinner.start();
@@ -25,7 +26,7 @@ int main(int argc, char ** argv)
     // 3 |  from Topic      (set topicName)
 
     CameraWorkingInfo info;
-    int freq = 10;          // in hz (image updating frequency)
+    int freq = 20;          // in hz (image updating frequency)
     double accurancy = 0.1; // accurancy of circle detection
 
     // Read ROS parameters
@@ -50,6 +51,8 @@ int main(int argc, char ** argv)
 
     SimpleCV cv(nh, info, freq);
     cv::Mat image;
+    int ch ;
+    initscr();
     while(ros::ok()) {
         image = cv.getImage().clone();
         if (!image.empty()) {
@@ -57,9 +60,9 @@ int main(int argc, char ** argv)
             // Recognize circle with some accurancy
             circles = findCircles(image, minColor, maxColor, accurancy,
                 DILATION_SIZE, BLUR_SIZE, circleColor, contourColor);
-            cout<<circles.center[0]<<" "<<circles.center[0]<<" "circles.center[0]<<"\n";
 
-            if (circles.size() > 0)
+            if (circles.size() > 0 && )
+
                 circlePublisher.publish(circles[0]);
 
             #ifdef DEBUG
@@ -67,7 +70,27 @@ int main(int argc, char ** argv)
             #endif
         }
         cv::waitKey(1);
+
+        ar.drone_move(x,y,z,zz);
     }
 
     ros::waitForShutdown();
+}
+
+void takeoffCallback(const std_msgs::String::ConstPtr  msg))
+{
+
+    try {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        ros::shutdown();
+    }
+
+    image = cv_ptr->image;
+    // if (camWidth == 0 || camHeight == 0) {
+    //     camWidth = image.cols; camHeight = image.rows;
+    // }
+    // ros::Duration(round(1/freq)).sleep();
 }

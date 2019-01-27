05af9a454d887e272666916e33bd16ad011348b9
ardrone <CherginetsDA>
2019-01-26 20:19:07 -0500

    add subscribe for navdata

--- ardrone_system/include/ardrone_system/ardrone.h
+++ ardrone_system/include/ardrone_system/ardrone.h
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Empty.h>
+#include <ardrone_autonomy/Navdata.h>
 
 
 class drone{
@@ -10,19 +11,39 @@ private:
 	ros::Publisher takeoff_publisher;
 	ros::Publisher land_publisher;
 	ros::Publisher reset_publisher;
+	ros::Subscriber navdata_sub;
+	int state,altd;
+	float batteryPercent;
+	std::vector<float> rotXYZ;
+
 	geometry_msgs::Twist move;
 	std_msgs::Empty empty;
-	double start_time_sleeping = 1.5;
+	double start_time_sleeping;
+
+	inline void navdata_callback(const ardrone_autonomy::Navdata::ConstPtr& msg){
+  		state = msg -> state;
+  		altd = msg -> altd;
+  		batteryPercent = msg -> batteryPercent;
+  		rotXYZ[0] = msg -> rotX;
+  		rotXYZ[1] = msg -> rotY;
+  		rotXYZ[2] = msg -> rotZ;
+	}
+
 public:
+
+	
+
 	drone(){
 		move_publisher = n.advertise<geometry_msgs::Twist>("cmd_vel",1);
 		takeoff_publisher = n.advertise<std_msgs::Empty>("ardrone/takeoff",1);
 		land_publisher = n.advertise<std_msgs::Empty>("ardrone/land",1);
 		reset_publisher = n.advertise<std_msgs::Empty>("ardrone/reset",1);
+		navdata_sub = n.subscribe("ardrone/navdata", 1, &drone::navdata_callback, this);
+		start_time_sleeping = 1.5;
 	}
 
 	void drone_move(double x, double y, double z, double zz){
-		if(n.ok()){
+		if(ros::ok()){
 			move.linear.x = x;
 			move.linear.y = y;
 			move.linear.z = z;
@@ -46,6 +67,27 @@ public:
 
 	inline void set_start_time_sleeping(double t){
 		start_time_sleeping = t;
+	}
+
+	float battery(){
+		return batteryPercent;
+	}
+
+	std::vector<float> rotxyz(){
+		return rotXYZ;
+	}
+
+	inline bool ok(){
+		return ros::ok();
+	}
+	inline bool inflight(){
+
+		if (state == 3 || state == 7){
+			return true;	
+		} else {
+			return false;
+		}
+		
 	}
 
 };
--- ardrone_system/src/ardrone.cpp
+++ ardrone_system/src/ardrone.cpp
@@ -1,55 +1,68 @@
 #include <ardrone_system/ardrone.h>
 #include <ncurses.h>
+#include <ctime> 
+
+// #include <DroneController.h>
+// #include <SimpleCV.hpp>
+// #include <algorithms.hpp>
+
+// #include <opencv2/imgproc.hpp>
+// #include <opencv2/highgui.hpp>
+
+int DILATION_SIZE = 0;
+int BLUR_SIZE = 0;
+
+// cv::Scalar circleColor(255, 255, 255), contourColor(0, 255, 0);
 
 int main(int argc, char **argv){
-	int ch ;
+	double ch = 0 ;
 	initscr();
-	float x,y,z,xx,yy,zz, k = 0.5;
+	float x,y,z,zz,k = 0.5;
 	ros::init(argc, argv, "ardrone_move");
 	drone ar;
-	ar.takeoff();
-	while (1) {
-		ch = getch();
-		if (ch == 'w'){
-			x=k;
-			}
-		if (ch == 'd'){
-			y=-k;
-			}
-		if (ch == 's'){
-			x=-k;
+	noecho();
+	halfdelay(0.1); 
+	// DroneController dronectrl(0.802, 0.1, 0.1, 0.1, 1280.0, 720.0);
+	// Eigen::Vector3d zyrPos;
+	// Eigen::Vector3d desVector={1, 0, -1};
+	// Eigen::Vector3f ctrl;
+	// std::vector<ar_cv::CircleInfo> circles;
+	// cv::Mat image;
+	// std::vector<int> mincolor(3)=(18,102,123), maxcolor(3)=(36,223,255);
+	// cv::Scalar minColor(mincolor[0], mincolor[1], mincolor[2]),
+ //               maxColor(maxcolor[0], maxcolor[1], maxcolor[2]);
+	
+	while (ar.ok()) {
+		// image = cv.getImage().clone();
+  //       if (!image.empty()) {
+  //       	circles = findCircles(image, minColor, maxColor, accurancy, DILATION_SIZE, BLUR_SIZE, circleColor, contourColor);
+
+  //           if (circles.size() > 0)
+  //               zyrPos={circles[0][2],circles[0][1],circles[1]}
+
+  //           #ifdef DEBUG
+  //           cv::imshow("Image", image);
+  //           #endif
+  //       }
+  //       cv::waitKey(1);
+		switch (getch()){
+			case 't':
+				ar.takeoff();	
+			break;
+			case 'l':
+				ar.land();
+				endwin();
+				return 0;
+			break;
 		}
-		if (ch == 'a'){
-			y=k;
+		if (ar.inflight()){
+			printw("I'm fly!");
+		} else {
+			printw("I'm on the graund!");
 		}
-		if (ch == 'e'){
-			zz=k;
-		}
-		if (ch == 'q'){
-			zz=-k;
-		}
-		if ((ch == 'r') && k < 1) {
-			k+=0.1;
-		}
-		if ((ch == 'f') && k > 0) {
-			k-=0.1;
-		}
-		if (ch == 't'){
-			z=k;
-		}
-		if (ch == 'g'){
-			z=-k;
-		}
-		if (ch == 'x'){
-			ar.land();
-			endwin();
-		}
-		std::cout<<ch;
-		ar.drone_move(x,y,z,zz);
-		x = 0;
-		y = 0;
-		z = 0;
-		zz = 0;
+		// ctrl=(Eigen::Vector3f)dronectrl.getVectorControl(1, zyrPos, 0, 0, desVector);
+		// ar.drone_move(ctrl[0],ctrl[1],ctrl[2],0.00);
+	
 	}
 
 	ros::spin();
